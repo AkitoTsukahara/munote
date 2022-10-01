034cc6b20fcb3233882f8617dc6bd8a17d651231
AkitoTsukahara <a-tsukahara@macloud.jp>
2022-10-01 23:47:19 +0900

    🎭setup playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,10 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
+	reporter: 'html',
+	retries: 3,
+	expect: {
+		timeout: 20000
 	}
 };
 
