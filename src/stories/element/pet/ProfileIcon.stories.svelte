37b37cf641a671d89c2a3d751530031ed420dc09
AkitoTsukahara <a-tsukahara@macloud.jp>
2022-10-03 03:04:26 +0900

    🗓️📸update profileIcon component

--- src/stories/element/pet/ProfileIcon.stories.svelte
+++ src/stories/element/pet/ProfileIcon.stories.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
-	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
-	import ProfileIcon from '$lib/components/element/pet/ProfileIcon.svelte';
-	import profileImage from '../../assets/images/profile-image.jpg';
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+  import ProfileIcon from '$lib/components/element/pet/ProfileIcon.svelte'
+  import profileImage from '../../assets/images/profile-image.jpg'
 </script>
 
 <Meta title="Components/Element/Pet/ProfileIcon" component={ProfileIcon} />
 
 <div>
-	<Template let:args>
-		<ProfileIcon {...args} />
-	</Template>
+  <Template let:args>
+    <ProfileIcon {...args} />
+  </Template>
 </div>
 
 <Story
-	name="Default"
-	args={{
-		profileImage: profileImage,
-		name: 'ムート'
-	}}
+  name="Default"
+  args={{
+    profileImage: profileImage,
+    name: 'ムート'
+  }}
 />
