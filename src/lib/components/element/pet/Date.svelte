f2b33e5c2af49fe79e3c75e4bde81f4fb33c2fef
AkitoTsukahara <a-tsukahara@macloud.jp>
2022-10-03 03:02:58 +0900

    📆add date components

---
+++ src/lib/components/element/pet/Date.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { format } from 'date-fns'
+
+  export let date: string | null
+  export let title: string
+
+  $: formatedDate = date ? format(new Date(date), 'yyyy年MM月dd日') : null
+</script>
+
+<div>
+  <span class="title">{title}</span>
+  <span class="date">{formatedDate}</span>
+</div>
+
+<style lang="scss">
+  div {
+    display: grid;
+    font-size: 14px;
+
+    .title {
+      font-weight: bold;
+    }
+  }
+</style>
---
+++ src/stories/element/pet/Date.stories.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+  import Date from '$lib/components/element/pet/Date.svelte'
+</script>
+
+<Meta title="Components/Element/Pet/Date" component={Date} />
+
+<div>
+  <Template let:args>
+    <Date {...args} />
+  </Template>
+</div>
+
+<Story
+  name="Default"
+  args={{
+    title: '誕生日',
+    date: '2021-10-25T12:00:00+09:00'
+  }}
+/>
