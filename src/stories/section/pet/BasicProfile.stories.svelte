c22202d8e73ce6046b7b18f0b61855cf707811fd
AkitoTsukahara <a-tsukahara@macloud.jp>
2022-10-03 10:38:38 +0900

    🍊add profile component

---
+++ src/stories/section/pet/BasicProfile.stories.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import ProfileIcon from '$lib/components/element/pet/ProfileIcon.svelte'
+  import Anniversary from '$lib/components/block/pet/Anniversary.svelte'
+</script>
+
+<div class="basic-profile">
+  
+</div>
+
+<style lang="scss">
+
+</style>
