dad34d6bfbe0b05e1652d6994e7451f4826adb02
AkitoTsukahara <a-tsukahara@macloud.jp>
2022-10-05 00:51:19 +0900

    🍓add top page

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,23 @@
+<div class="layout">
+  <div class="header">
+
+  </div>
+
+  <div class="container">
+    <slot />
+  </div>
+  <div class="footer">
+
+  </div>
+</div>
+
+<style lang="scss">
+  .layout {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
+  .container {
+    flex-grow: 1;
+  }
+</style>
---
+++ src/routes/(app)/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { usePet } from '$lib/stores/pet/store'
+  import type { GetOutput } from './+page.server'
+  import BasicProfile from '$lib/components/section/pet/BasicProfile.svelte'
+
+  export let data: GetOutput
+  const {petStore, setFromApi} = usePet()
+  $: setFromApi(data.pet)
+
+  $: pet = $petStore
+</script>
+
+<div>
+  <BasicProfile
+      name="{pet.name}"
+      breed="{pet.breed}"
+      birthday="{pet.birthday}"
+      sex="{pet.sex}"
+      profileImage="{pet.profileImage.url}"
+      familyDay="{pet.familyday}"
+  />
+</div>
+
