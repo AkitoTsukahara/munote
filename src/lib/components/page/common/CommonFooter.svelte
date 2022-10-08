7a38e70961b4a27fbe0c5ab58826f208e02d80cf
AkitoTsukahara <a-tsukahara@macloud.jp>
2022-10-09 00:29:22 +0900

    👣add footer

---
+++ src/lib/components/page/common/CommonFooter.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  import Calendar from "$lib/components/icon/Calendar.svelte"
+  import Chart from "$lib/components/icon/Chart.svelte"
+  import Diary from "$lib/components/icon/Diary.svelte"
+  import Money from "$lib/components/icon/Money.svelte"
+  import Pet from "$lib/components/icon/Pet.svelte"
+</script>
+
+<div class="footer-icon-list">
+    <div class="footer-icon">
+        <Pet/>
+        <div class="icon-title">ペット</div>
+    </div>
+    <div class="footer-icon">
+        <Chart/>
+        <div class="icon-title">チャート</div>
+    </div>
+    <div class="footer-icon">
+        <Diary/>
+        <div class="icon-title">日記</div>
+    </div>
+    <div class="footer-icon">
+        <Money/>
+        <div class="icon-title">支出</div>
+    </div>
+    <div class="footer-icon">
+        <Calendar/>
+        <div class="icon-title">カレンダー</div>
+    </div>
+</div>
+
+<style lang="scss">
+    .footer-icon-list {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+    .footer-icon {
+        display: grid;
+        justify-content: center;
+        :global(svg) {
+            width: 40px;
+            height: 40px;
+            margin: 0 auto;
+        }
+    }
+    .icon-title {
+        font-size: 12px;
+        text-align: center;
+    }
+</style>
---
+++ src/stories/page/common/CommonFooter.stories.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+    import CommonFooter from '$lib/components/page/common/CommonFooter.svelte'
+  </script>
+  
+  <Meta title="Components/Page/Common/CommonFooter" component={CommonFooter} />
+  
+  <div>
+    <Template let:args>
+      <CommonFooter {...args} />
+    </Template>
+  </div>
+  
+  <Story
+    name="Default"
+    args={{
+      title: '誕生日',
+      date: '2021-10-25T12:00:00+09:00'
+    }}
+  />
+  
