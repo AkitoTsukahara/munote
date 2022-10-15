b54fa1c83785059f2003a8dc79e6101a50fe7329
AkitoTsukahara <a-tsukahara@macloud.jp>
2022-10-15 13:16:34 +0900

    📃create ListText story

--- src/lib/components/element/diary/ListText.svelte
+++ src/lib/components/element/diary/ListText.svelte
@@ -13,6 +13,8 @@
     width: 100%;
     overflow: hidden;
     text-overflow: ellipsis;
-    white-space: nowrap;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
   }
 </style>
