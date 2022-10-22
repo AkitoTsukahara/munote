301d1adf64440bb0e670f5e07b9e6241a867422a
AkitoTsukahara <a-tsukahara@macloud.jp>
2022-10-22 23:51:59 +0900

    🧦create FooterMenu component

---
+++ src/lib/components/block/diary/detail/FooterMenu.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import Garbage from '$lib/components/icon/Garbage.svelte'
+  import Share from '$lib/components/icon/Share.svelte'
+
+  export let id: string
+
+  function deleteDiary() {
+    let check = confirm('この日記を削除してもよろしいですか？')
+    if (check) {
+      // TODO 削除機能
+      location.href = '/diary'
+    }
+  }
+
+  function shareDiary() {
+    alert('シェア機能は準備中です👷')
+  }
+</script>
+
+<div class="footer-menu">
+  <div class="garbage" on:click={deleteDiary}>
+    <Garbage />
+  </div>
+  <div class="share" on:click={shareDiary}>
+    <Share />
+  </div>
+</div>
+
+<style lang="scss">
+  .footer-menu {
+    display: flex;
+    justify-content: space-between;
+    background: $card-background;
+    color: $paragraph;
+    padding: 8px;
+  }
+  .garbage,
+  .share {
+    width: 16px;
+    height: 16px;
+  }
+</style>
