d8f61c4d4403c55bf853778d3ac0ac9ffebc5e79
AkitoTsukahara <a-tsukahara@macloud.jp>
2022-10-22 22:44:54 +0900

    create HederMennu component

---
+++ src/stories/block/diary/detail/HeaderMenu.stories.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+  import HeaderMenu from '$lib/components/block/diary/detail/HeaderMenu.svelte'
+</script>
+
+<Meta title="Components/Block/Diary/Detail/HeaderMenu" component={HeaderMenu} />
+
+<div>
+  <Template let:args>
+    <HeaderMenu {...args} />
+  </Template>
+</div>
+
+<Story
+    name="Default"
+    args={{
+    id: 'recZyqtDllgSky0v8',
+    date: '2021-10-25T12:00:00+09:00',
+  }}
+/>
+
+<style lang="scss">
+  div {
+    width: 360px;
+  }
+</style>
