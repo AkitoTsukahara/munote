16be1da22b9458cfba5a1550947c5cc45e1dcd22
AkitoTsukahara <a-tsukahara@macloud.jp>
2022-10-28 23:32:56 +0900

    📝add TextArea Components

---
+++ src/lib/components/element/diary/[id]/edit/TextArea.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  export let contents: string
+</script>
+
+<textarea name="contents" cols="40" rows="10">{contents}</textarea>
+
+<style lang="scss">
+  textarea {
+    display: block;
+    width: 100%;
+    padding: 4px 16px;
+    border-radius: 4px;
+    border: none;
+    box-shadow: 0 0 0 1px $card-background inset;
+    appearance: none;
+    resize: vertical;
+
+    &:focus {
+      outline: 0;
+      box-shadow: 0 0 0 2px rgb(33, 150, 243) inset;
+    }
+  }
+</style>
---
+++ src/stories/element/diary/[id]/edit/TextArea.stories.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+  import TextArea from '$lib/components/element/diary/[id]/edit/TextArea.svelte'
+</script>
+
+<Meta title="Components/Element/Diary/[ID]/Edit/TextArea" component={TextArea} />
+
+<div>
+  <Template let:args>
+    <TextArea {...args} />
+  </Template>
+</div>
+
+<Story
+    name="Default"
+    args={{
+    contents: 'テキストが入ります。テキストが入ります。\n'+
+'テキストが入ります。テキストが入ります。\n'+
+'テキストが入ります。'
+  }}
+/>
+
+<style lang="scss">
+  div {
+    width: 400px;
+    height: 360px;
+  }
+</style>
