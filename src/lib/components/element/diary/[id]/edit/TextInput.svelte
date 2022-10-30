f4f57492acbb06eef5c867c03f125b1e5a913958
AkitoTsukahara <a-tsukahara@macloud.jp>
2022-10-30 23:53:10 +0900

    ✏️create inputText component

---
+++ src/lib/components/element/diary/[id]/edit/TextInput.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  export let title=''
+</script>
+
+<input type="text" bind:value={title} placeholder="タイトルを入力してください">
+
+<style lang="scss">
+  input[type="text"] {
+    font: 15px/24px sans-serif;
+    box-sizing: border-box;
+    width: 100%;
+    padding: 0.3em;
+    transition: 0.3s;
+    letter-spacing: 1px;
+    color: $button-text;
+    border: 1px solid #1b2538;
+    border-radius: 4px;
+
+    &:focus {
+      border: 1px solid $button-text;
+      outline: none;
+      box-shadow: 0 0 5px 1px rgba($paragraph, .5);
+    }
+  }
+</style>
---
+++ src/stories/element/diary/[id]/edit/TextInput.stories.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+  import TextInput from '$lib/components/element/diary/[id]/edit/TextInput.svelte'
+</script>
+
+<Meta title="Components/Element/Diary/[ID]/Edit/TextInput" component={TextInput} />
+
+<div>
+  <Template let:args>
+    <TextInput {...args} />
+  </Template>
+</div>
+
+<Story
+    name="Default"
+    args={{
+    title: 'タイトルが入ります。'
+  }}
+/>
+
+<style lang="scss">
+  div {
+    width: 400px;
+    height: 360px;
+  }
+</style>
